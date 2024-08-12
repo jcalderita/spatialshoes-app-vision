89695936cb78e8611b3d943fd0ecd702ea1318ea
Jorge Calderita <jcalderita@outlook.com>
2024-08-12 13:50:10 +0200

    refactor: Change rotate transform for volumetric shoe
    - Change rotation3DEffect for transform

--- SpatialShoes/ShoeVolumetricView.swift
+++ SpatialShoes/ShoeVolumetricView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Jorge Calderita on 11/8/24.
 //
-
 import SwiftUI
 import RealityKit
 import RealityShoeGallery
@@ -16,40 +15,54 @@ struct ShoeVolumetricView: View {
     @State private var rotationAxis: (x: CGFloat, y: CGFloat, z: CGFloat) = (x: 0, y: 0, z: 0)
     @State private var shoeModel: Entity?
     
+    @State private var currentAngleX: Float = 0.0
+    @State private var currentAngleY: Float = 0.0
+    
     var body: some View {
         Group {
-            Text(model3DName)
             if let shoeModel {
                 RealityView { content in
                     shoeModel.name = model3DName
                     shoeModel.scale = SIMD3(repeating: 0.03)
-                    shoeModel.generateCollisionShapes(recursive: false)
+                    shoeModel.generateCollisionShapes(recursive: true)
                     shoeModel.components.set(InputTargetComponent())
                     content.add(shoeModel)
                 }
-                .gesture(
-                    DragGesture()
-                        .targetedToAnyEntity()
-                        .onChanged { value in
-                            print(value)
-                            let angle = sqrt(pow(value.translation.width, 2) + pow(value.translation.height, 2))
-                            rotation = Angle(degrees: Double(angle))
-                            
-                            let axisX = -value.translation.height / CGFloat(angle)
-                            let axisY = value.translation.width / CGFloat(angle)
-                            rotationAxis = (x: axisX, y: axisY, z: 0)
-                        }
-                )
-//                .rotation3DEffect(rotation, axis: rotationAxis)
+                .gesture(DragGesture().onChanged { gesture in
+                    handleRotation(value: gesture) { rotation in
+                        shoeModel.transform.rotation = rotation
+                    }
+                })
+            } else {
+                CustomProgressView("Load volumetric shoe")
             }
         }
         .task {
             shoeModel = try? await Entity(named: model3DName, in: realityShoeGalleryBundle)
         }
     }
+    
+    private func handleRotation(value: DragGesture.Value, updateRotation: (simd_quatf) -> Void) {
+        let dragAmountX = Float(value.translation.height)
+        let dragAmountY = Float(value.translation.width)
+        
+        let angleDeltaX = dragAmountX / 500.0
+        let angleDeltaY = dragAmountY / 500.0
+        
+        currentAngleX += angleDeltaX
+        currentAngleY += angleDeltaY
+        
+        let rotationX = simd_quatf(angle: currentAngleX, axis: SIMD3(1, 0, 0))
+        let rotationY = simd_quatf(angle: currentAngleY, axis: SIMD3(0, 1, 0))
+        
+        updateRotation(rotationY * rotationY)
+    }
 }
 
 #Preview {
     ShoeVolumetricView(model3DName: ShoeModel.firstTest.model3DName)
-//        .WindowStyle(.volumetric)
+}
+
+#Preview("Two") {
+    ShoeVolumetricView(model3DName: ShoeModel.firstTest.model3DName)
 }
